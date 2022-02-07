ba4c214a589630c6807981c2dd78b264f24bf653
Emmanuel Hernández Maqueda <emmanuel.hernandezm@softtek.com>
2022-02-07 02:24:19 -0600

    MERN CalendarApp FrontEnd

---
+++ build/precache-manifest.2914558b43f059dc6f3861763dc2397e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb04fcd4800e5df68da97d6582185233",
+    "url": "/index.html"
+  },
+  {
+    "revision": "048386bff12490a5ff82",
+    "url": "/static/css/2.019d3f75.chunk.css"
+  },
+  {
+    "revision": "9090fd8b3f00de596ce6",
+    "url": "/static/css/main.98fa951e.chunk.css"
+  },
+  {
+    "revision": "048386bff12490a5ff82",
+    "url": "/static/js/2.e32f517e.chunk.js"
+  },
+  {
+    "revision": "6f928bff4b1f2017db2882f01704244a",
+    "url": "/static/js/2.e32f517e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9090fd8b3f00de596ce6",
+    "url": "/static/js/main.5025de0e.chunk.js"
+  },
+  {
+    "revision": "7e829d7f4f2411c25d99",
+    "url": "/static/js/runtime-main.8c2230a5.js"
+  }
+]);
